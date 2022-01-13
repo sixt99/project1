b7b7b3088f8d8c1b88fa51ffb00dcbf4c7b393b9
Sixte Oriol <soriol@vipdistrict.com>
2022-01-13 20:05:54 +0100

    Commit de prova sense token

---
+++ project1/src/project1/Hola.java
@@ -0,0 +1,24 @@
+package project1;
+
+public class Hola {
+
+	String casa;
+	int finestres;
+
+	public String getCasa() {
+		return casa;
+	}
+
+	public int getFinestres() {
+		return finestres;
+	}
+
+	public void setCasa(String casa) {
+		this.casa = casa;
+	}
+
+	public void setFinestres(int finestres) {
+		this.finestres = finestres;
+	}
+
+}
--- project1/src/project1/Main.java
+++ project1/src/project1/Main.java
@@ -3,7 +3,17 @@ package project1;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		Hola hola = new Hola();
+		hola.setCasa("Casa");
+		hola.setFinestres(5);
+		Hola1 hola1 = new Hola1();
+		hola1.setCasa("Casa");
+		hola1.setFinestres(5);
+
+		if (hola.equals(hola1))
+			System.out.println("T'estimo.");
+		else
+			System.out.println("Et fots.");
 	}
 
 }
